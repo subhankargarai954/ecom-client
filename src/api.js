5f94ce48e77340fba4d8126b7664996354433ec3
Subhankar Garai <subhankargarai1998@gmail.com>
2026-06-21 16:29:32 +0530

    REACT_APP_BACKEND_BASE_URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const BASE = process.env.REACT_APP_BACKEND_BASE_URL;
 
 const api = axios.create({ baseURL: BASE });
 
